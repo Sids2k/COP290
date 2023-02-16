b9e0c4f007bf9f5f05c158fa4b1e1f4698325197
navya19j <navya19j@gmail.com>
2023-02-17 01:06:57 +0530

    fix segmentation fault

--- Assignment2-COP290/part1/src/main.c
+++ Assignment2-COP290/part1/src/main.c
@@ -7,27 +7,27 @@
 #include <ucontext.h>
 #include <stdbool.h>
 
-static ucontext_t ctx;
+static ucontext_t ctx, ctx1;
 
 static void conjecture(int len, void* options, int sz, void fn(void*)){
 	int i = -1;
+	// printf("%d\n", i);
 	getcontext(&ctx); // Save main context
 	i++;
 	if (i == len) {
 		return;
 	}
-	static ucontext_t ctx1;
 	getcontext(&ctx1); // Save temp context
 	ctx1.uc_stack.ss_sp = malloc(MEM); 
-	ctx1.uc_stack.ss_size = sz;
-	ctx1.uc_link = 0; 
+	ctx1.uc_stack.ss_size = MEM;
+	ctx1.uc_link = &ctx; 
 	makecontext(&ctx1, fn, 1, options + i * sz); // temp context calls fn
 	setcontext(&ctx1); // start the temp context
 } // Create context and start traversal
 
 void assert(bool b){
 	if(!b) {
-		// printf("Assertion failed\n");
+		printf("Assertion failed\n");
 		setcontext(&ctx); // Restore context if condition fails
 	}
 } // Restore context if condition fails
@@ -50,6 +50,7 @@ int nested(int i) {
 }
 
 void app(void* c) {
+	// printf("In app");
 	int* i = (int*)c;
 	// printf("Trying %d\n", *i);
 	assert(is_lt_40(*i));
@@ -58,10 +59,11 @@ void app(void* c) {
 }
 
 int main(void) {
-	int mynums[] = {39, 11, 23, 42, 41, 52, 22};
+	int mynums[] = {25, 39, 11, 23, 42, 41, 52, 22};
+	// printf("Trying");
 	// We have to ensure that conjecture lives in the bottom of the call stack. 
 	// If the conjecture frame is popped, we will never be able to rollback to it.
-	conjecture(7, (void*) mynums, sizeof(int), &app);
+	conjecture(8, (void*) mynums, sizeof(int), &app);
 }
 
 
