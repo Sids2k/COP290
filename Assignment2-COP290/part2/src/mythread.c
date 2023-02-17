0c614b1cad33e59e9ec1e465bed3784119315d57
navya19j <navya19j@gmail.com>
2023-02-18 01:31:59 +0530

    fix_thread_lib

--- Assignment2-COP290/part2/src/mythread.c
+++ Assignment2-COP290/part2/src/mythread.c
@@ -1,6 +1,7 @@
 // Make custom thread library
 #include "mythread.h"
 #include "list.h"
+#define MEM 64000
 #include <hm.h>
 #include <list.h>
 
@@ -16,22 +17,26 @@ void* mythread_create(void func(void*), void* arg){
     // set up context
     ucontext_t ctx;
     getcontext(&ctx);
-    ctx.uc_stack.ss_sp = malloc(STACK_SIZE);
-    ctx.uc_stack.ss_size = STACK_SIZE;
+    ctx.uc_stack.ss_sp = malloc(MEM);
+    ctx.uc_stack.ss_size = MEM;
     // uc_link to main context
     ctx.uc_link = &main_ctx;
     // make context
     makecontext(&ctx, func, 1, arg);
 
     // add to list
-    list_add(l, ctx);
+    list_add(l, &ctx);
 }
 
 // mythread_join function
 void mythread_join(){
     // add a loop
-    while(!list_is_empty(l)){
-        ucontext_t temp = list_remove(l);
+    while(!is_empty(l)){
+        // get element from list
+        struct listentry* le = l->head;
+        ucontext_t temp = *(ucontext_t*) le->data;
+        // remove from list
+        list_rm(l, &temp);
         swapcontext(&main_ctx, &temp);
     }
 }
