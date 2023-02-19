48381ced6e601b1cc6595a21cff948c50a283be7
Navya Jain <67495793+navya19j@users.noreply.github.com>
2023-02-19 17:29:56 +0530

    fix threads_

--- Assignment2-COP290/part2/src/mythread.c
+++ Assignment2-COP290/part2/src/mythread.c
@@ -8,6 +8,8 @@
 #define MEM 64000
 
 static ucontext_t main_ctx;
+struct listentry* curr_le;
+
 struct list *l;
 
 void mythread_init() {
@@ -17,43 +19,64 @@ void mythread_init() {
 // mythread_create function
 ucontext_t* mythread_create(void func(void*), void* arg){
     // set up context
-    static ucontext_t ctx;
+    getcontext(&main_ctx);
+    // printf("Inside mythread_create");
+    ucontext_t* ctx = (ucontext_t*) malloc(sizeof(ucontext_t));
+    // static ucontext_t ctx;
     // add to list
-    list_add(l, &ctx);
-    getcontext(&ctx);
-    ctx.uc_stack.ss_sp = malloc(MEM);
-    ctx.uc_stack.ss_size = MEM;
+    list_add(l, ctx);
+    getcontext(ctx);
+    (*ctx).uc_stack.ss_sp = malloc(MEM);
+    (*ctx).uc_stack.ss_size = MEM;
     // uc_link to main context
-    ctx.uc_link = &main_ctx;
-    // make context
-    makecontext(&ctx, func, 1, arg);
+    (*ctx).uc_link = &main_ctx;
+    makecontext(ctx, func, 1, arg);
 
-    return &ctx;
+    return ctx;
+}
+
+struct listentry* get_next(struct list* l, struct listentry* e)
+{
+    if(e->next != NULL)
+        return e->next;
+    else
+        return l->head;
 }
 
 // mythread_join function
 void mythread_join(){
     // get the head
-    struct listentry* le = l->head;
-    // loop through the list
-    while(le != NULL){
+    // struct listentry* le = l->head;
+    curr_le = l->head;
+
+    // check if list is empty
+    while (!is_empty(l)){
         // get the context
-        ucontext_t* temp = (ucontext_t*) le->data;
+        ucontext_t* temp = (ucontext_t*) curr_le->data;
+        // curr_le = le;
         // swap context to 
         swapcontext(&main_ctx, temp);
-        // get next element
-        le = le->next;
+        // remove the element
+        struct listentry* to_rem = curr_le;
+        curr_le = get_next(l, curr_le);
+        list_rm(l, to_rem);
     }
 }
 
 // mythread_yield function
 void mythread_yield(){
-    // get element from list
-    struct listentry* le = l->head;
-    ucontext_t temp = *(ucontext_t*) le->data;
-    // get next element
-    struct listentry* le1 = le->next;
-    ucontext_t temp1 = *(ucontext_t*) le1->data;
+
+    // see what the current context is
+    ucontext_t* curr_ctx = (ucontext_t*) curr_le->data;
+
+    // get the next element
+    struct listentry* next_le = get_next(l, curr_le);
+
+    // get the next context
+    ucontext_t* next_ctx = (ucontext_t*) next_le->data;
+
+    curr_le = next_le;
+
     // swap context
-    swapcontext(&temp, &temp1);
+    swapcontext(curr_ctx, next_ctx);
 }
